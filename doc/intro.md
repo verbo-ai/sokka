9e69d72d0977c55aa9545366cd6cad4353134aea
Sathyavijayan Vittal <sathyavijayan@gmail.com>
2021-04-26 17:25:20 +0100

    new structure

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to sokka
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject verbo/sokka "0.0.1"
-  :description "Task management and async utilities for Clojure(Script)"
-  :url "https://github.com/verbo-ai/sokka"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :profiles {:uberjar {:aot :all}})
